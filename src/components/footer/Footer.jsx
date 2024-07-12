44a91a352abac27e6530a7a199c3c1deb152afa4
raneem <raneem.hamzah@gmail.com>
2024-07-12 11:58:22 +0300

    edit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                 <div className='out-circle'><a href="https://api.whatsapp.com/send?phone=966542956307" target='_blank'>
                     <i className="fa-brands fa-whatsapp"></i>
                 </a></div>
-                <div className='out-circle'><a href="https://www.linkedin.com/in/raneem-hamzah-948b91113/" target='_blank'>
+                <div className='out-circle'><a href="https://www.linkedin.com/in/raneem-hamzah-83a201319?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app " target='_blank'>
                     <i className="fa-brands fa-linkedin-in"></i>
                 </a></div>
                 <div className='out-circle'><a href="https://t.me/raneem_ham" target='_blank'>
